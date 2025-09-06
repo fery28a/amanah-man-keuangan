1c9b34c2f8c35fb67ad7741f425ad9235f62a14c
fery28a <masferyandika28@gmail.com>
2025-09-06 17:31:38 +0700

    Update piutangRoutes.js

--- amanah-keuangan-backend/src/routes/piutangRoutes.js
+++ amanah-keuangan-backend/src/routes/piutangRoutes.js
@@ -1,9 +1,15 @@
-// src/routes/piutangRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getPiutang, addPiutang, updatePiutang, deletePiutang } = require('../controllers/piutangController');
+const { 
+  getPiutang, 
+  addPiutang, 
+  updatePiutang, 
+  deletePiutang, 
+  getPiutangLunas 
+} = require('../controllers/piutangController');
 
 router.route('/').get(getPiutang).post(addPiutang);
+router.route('/lunas').get(getPiutangLunas);
 router.route('/:id').put(updatePiutang).delete(deletePiutang);
 
 module.exports = router;
